a7949247be7631d34cd1d82c701e6421e10332f8
vikas Bhnadari <143922604+Vikaskumarrr@users.noreply.github.com>
2024-07-19 02:36:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.getElementById("button").addEventListener("click", async ()=>{
+
+	let text = document.getElementById("text").value;
+	let delay = document.getElementById("delay").value;
+
+	if(text&&delay){
+		let myPromise = new Promise((resolve, reject)=>{
+			sertTimeout(()=>{
+				resolve(text);
+			},delay)
+		})
+
+		let text2 = await myPromise;
+		if(text2){ 
+			document.getElementById("output").textContent = text2;	
+		}
+	}
+	
+	
+})
